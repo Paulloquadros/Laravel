6bb05e1094a56cd0d4214af5a4461426200e23c8
Paullo Quadros <paulloquadros@gmail.com>
2022-01-15 15:04:40 -0300

    primeiro commit

---
+++ resources/views/db.blade.php
@@ -0,0 +1,2 @@
+<a>PAGINA DE DB<br></a>
+    <a href="/">RETORNAR</a>
---
+++ storage/framework/views/850ffbc50f996097eff516e21e91beb95799c2f2.php
@@ -0,0 +1,2 @@
+<a>PAGINA DE DB<br></a>
+    <a href="/">RETORNAR</a><?php /**PATH /home/ayala/TESTELARAVEL/resources/views/db.blade.php ENDPATH**/ ?>
