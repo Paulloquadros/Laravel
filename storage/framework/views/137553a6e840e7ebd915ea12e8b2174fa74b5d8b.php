bc66dd6b289fb7a0a78b935f30b95e0680dfe64e
paullo quadros <paulloquadros@gmail.com>
2022-01-20 22:23:22 -0400

    PRIMEIRO COMMIT EM CASA ^^^^^^^^^

---
+++ storage/framework/views/137553a6e840e7ebd915ea12e8b2174fa74b5d8b.php
@@ -0,0 +1,2 @@
+<a>PAGINA DE DB<br></a>
+    <a href="/">RETORNAR</a><?php /**PATH /home/paullo/TESTELARAVEL/resources/views/db.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b52094617b81c69fdfa4a4dfc05d555fa7a0a0da.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="LoginPage" content="width=device-width, initial-scale=1">
+
+        <title> LOGIN </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <head>
+            <style>
+                .a1 {
+                    color: blueviolet;
+                    background-color: red;
+                    text-align: center;
+                    text-shadow: 2px 2px black;
+                    
+                }
+                .log {
+                    background-color: gray;
+                    text-align: center;
+                }
+                .foot1 {
+                    margin-top: 16%;
+                    margin-left: 1%;
+                }
+                .container {
+                    text-align: center;
+                }
+            </style>
+
+        <h2 class="a1"> 
+            FACA LOGIN PARA CONTINUAR
+            </h2>
+
+        </head>
+
+        <body>
+
+        <h1 class="log">PAGINA DE LOGIN<h1>
+                <br>
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+                <br>
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+                <br>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+                <br>
+    <button type="submit">Login</button>
+    <button type="button" class="cancelbtn">Cancel</button>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+
+        </body>
+            <footer class="foot1">
+            
+            
+<?php
+echo 'this is a simple string';
+
+echo 'You can also have embedded newlines in
+strings this way as it is
+okay to do';
+
+// Outputs: Arnold once said: "I'll be back"
+echo 'Arnold once said: "I\'ll be back"';
+
+// Outputs: You deleted C:\*.*?
+echo 'You deleted C:\\*.*?';
+
+// Outputs: You deleted C:\*.*?
+echo 'You deleted C:\*.*?';
+
+// Outputs: This will not expand: \n a newline
+echo 'This will not expand: \n a newline';
+
+// Outputs: Variables do not $expand $either
+echo 'Variables do not $expand $either';
+?>
+
+
+    <a href="/"><img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/37948/preview.svg" width="50" height="25">RETORNAR</a>
+            </footer><?php /**PATH /home/paullo/TESTELARAVEL/resources/views/login.blade.php ENDPATH**/ ?>
